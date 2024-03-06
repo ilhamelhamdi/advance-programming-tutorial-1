f800fb59540a3f22edeb55a32f2ecaaa72533bc0
Hamdi <ilhamabdillah123@gmail.com>
2024-03-06 18:45:59 +0700

    [REFACTOR] Implement Map as main storage in Payment repository

--- src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/repository/PaymentRepository.java
@@ -5,35 +5,21 @@ import id.ac.ui.cs.advprog.eshop.model.Payment;
 import org.springframework.stereotype.Repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Repository
 public class PaymentRepository {
-    private final List<Payment> paymentData = new ArrayList<>();
+    private final Map<String,Payment> paymentData = new HashMap<>();
     public Payment save(Payment payment){
-        int i = 0;
-        for(Payment savedPayment: paymentData){
-            // Update
-            if(savedPayment.getId().equals(payment.getId())){
-                paymentData.remove(i);
-                paymentData.add(i,payment);
-                return payment;
-            }
-            i += 1;
-        }
-        // Create
-        paymentData.add(payment);
+        paymentData.put(payment.getId(), payment);
         return payment;
     }
     public Payment findById(String id){
-        for(Payment payment: paymentData){
-            if(payment.getId().equals(id)){
-                return payment;
-            }
-        }
-        return null;
+        return paymentData.get(id);
     }
     public List<Payment> findAll(){
-        return paymentData;
+        return new ArrayList<>(paymentData.values());
     }
 }
