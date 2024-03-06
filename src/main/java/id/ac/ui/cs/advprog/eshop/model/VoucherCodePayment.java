e88b2c5b37e07e177de3ace22d4e43005f1057f0
Hamdi <ilhamabdillah123@gmail.com>
2024-03-06 20:48:27 +0700

    [GREEN] Implement voucher code payment model

--- src/main/java/id/ac/ui/cs/advprog/eshop/model/VoucherCodePayment.java
+++ src/main/java/id/ac/ui/cs/advprog/eshop/model/VoucherCodePayment.java
@@ -10,5 +10,31 @@ import java.util.regex.Pattern;
 public class VoucherCodePayment extends Payment{
     VoucherCodePayment(Order order, Map<String, String> paymentData){
         super(order, PaymentMethod.VOUCHER_CODE.getValue(), paymentData);
+        if(!paymentData.containsKey("voucherCode")){
+            throw new IllegalArgumentException();
+        }
+
+        String voucherKey = paymentData.get("voucherCode");
+        Pattern pattern = Pattern.compile("\\\\d{8}");
+        Matcher matcher = pattern.matcher(voucherKey);
+        if(voucherKey.length() != 16){
+            setStatus(PaymentStatus.REJECTED.getValue());
+        } else if(!voucherKey.startsWith("ESHOP")){
+            setStatus(PaymentStatus.REJECTED.getValue());
+        } else if(!containsEightNumericCharacters(voucherKey)){
+            setStatus(PaymentStatus.REJECTED.getValue());
+        } else {
+            setStatus(PaymentStatus.SUCCESS.getValue());
+        }
+    }
+
+    private boolean containsEightNumericCharacters(String input) {
+        int numericCount = 0;
+        for (char c : input.toCharArray()) {
+            if (Character.isDigit(c)) {
+                numericCount++;
+            }
+        }
+        return numericCount == 8;
     }
 }
